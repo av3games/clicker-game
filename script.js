ffee68997c7d61ad417672def7e6b45bb4b1ad9a
av3games <72161973+av3games@users.noreply.github.com>
2020-10-30 14:13:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 money = 0
+// Sets the money per click and money amount.
 mpc = 1
+// Locks the money amount to 0.
 let moneyLock;
 moneyLock = true
+// Sets the title to the money amount for startup.
 document.title = `${money} Money | Clicker Game`
+// Sets the money graphic to the money amount.
 document.getElementById("money").innerHTML = `<span style="color:green;">Money: ${money}</span>`;
+// Startup money anti money cheat.
 if (money > 0 && moneyLock == true) {
     setTimeout(() =>   console.log(`Cheated money found, reseting money and disabling money lock. (${money})`), 1000)
   setTimeout(() => money = 0, 1000)
@@ -12,17 +17,19 @@ if (money > 0 && moneyLock == true) {
   setTimeout(() => console.log(`No cheated money found, disabling money lock. (${money})`), 1000)
   moneyLock = false;
 }
-
+// Button click function for the clicker.
 function cornerclicked() {
 money = money + mpc
 clickValid = true
 update();
 
 }
+// Text and title update function.
 function update() {
 document.getElementById("money").innerHTML = `<span style="color:green;">Money: ${money}</span>`;
 document.title = `${money} Money | Clicker Game`
 }
+// Extra loop function, ignore this.
 function loop() {
 document.title = `${money} Money | Clicker Game`
   loop();
